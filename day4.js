19b1b1b2e61a1c2378c804e83f974cb947d0d3ee
Aslam Ali <rks507469@gmail.com>
2021-01-04 22:12:53 +0530

    day6

--- day4.js
+++ day4.js
@@ -7,3 +7,16 @@ if(!Number.isNaN(theNumber)) {
 } else {
     console.log("You Naughty Naughty..... Playing with me!!")
 }
+
+// one liner if statements
+if(1+1 == 2) console.log("It's True");
+
+// if else ladder
+let num = Number("1000");
+if(num < 10) {
+    console.log("Small");
+} else if( num < 100) {
+    console.log("Medium");
+} else {
+    console.log("Large");
+}
---
+++ day5.js
@@ -0,0 +1,14 @@
+// introduction to loops, finally
+// let number = 0;
+// while (number <= 12) {
+//     console.log(number)
+//     number  = number + 2;
+// }
+
+let result = 1;
+let counter = 0;
+while (counter < 10) {
+    result = result * 2;
+    counter = counter + 1;
+}
+console.log(result);
