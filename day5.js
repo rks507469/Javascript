8fe1b5cb2bdd861226de4f7ff5fffe2ada43de1a
Aslam Ali <rks507469@gmail.com>
2021-01-05 18:36:25 +0530

    day 7

--- day5.js
+++ day5.js
@@ -5,10 +5,27 @@
 //     number  = number + 2;
 // }
 
+// let result = 1;
+// let counter = 0;
+// while (counter < 10) {
+//     result = result * 2;
+//     counter = counter + 1;
+// }
+// console.log(result);
+
+for(let number = 0; number <= 12; number = number + 2) {
+    console.log(number);
+}
+
 let result = 1;
-let counter = 0;
-while (counter < 10) {
+for(let counter = 0; counter < 10; counter = counter + 1) {
     result = result * 2;
-    counter = counter + 1;
 }
 console.log(result);
+
+for(let current = 20; ; current = current + 1) {
+    if(current % 7 == 0) {
+        console.log(current);
+        break;
+    }
+}
---
+++ day6.js
@@ -0,0 +1,9 @@
+// updating the values
+let counter = 0;
+
+counter += 1; // same as counter = counter + 1
+// counter++; => post increment
+counter *= 1; // same as counter = counter * 1
+counter /= 1; // same as counter = counter / 1
+counter -= 1; // same as counter = counter - 1
+// counter--; => post decrement
